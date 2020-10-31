e84b8b6ffbf851f1029e1e1a171082e0d0688fc1
513sojin <bovet.urssu@gamil.com>
2020-10-31 23:48:48 +0900

    page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div>ShareAdsLink</div>
+        <nav>
+            <li> <a href="#">Login</a></li>
+            <li> <a href="#">signin</a></li>
+        </nav>
       </header>
+      
+      <div className="wrapper">
+        
+        <div className="firstPage">
+          <div className="ment">
+            <p>광고 공유,</p>
+            <p>쉽고 간편하게.</p>
+            <p className="sub">어려운 홍보도 ShareAdsLink에서 쉽게 시작하세요.</p>
+          </div>
+          <div className="photo"></div>
+        </div>
+
+        <div className="secondPage">
+          <div className="ment">
+            <p>광고 공유,</p>
+            <p>쉽고 간편하게.</p>
+            <p className="sub">어려운 홍보도 ShareAdsLink에서 쉽게 시작하세요.</p>
+          </div>
+        </div>
+        <div className="thirdPage"></div>
+        <div className="footer"></div>
+      </div>
     </div>
   );
 }
---
+++ src/Component/AdContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AdContent(){
+    return(
+        <div>Adcontent!</div>
+    );
+}
+
+export default AdContent;
